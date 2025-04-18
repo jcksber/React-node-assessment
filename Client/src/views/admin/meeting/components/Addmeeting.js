d55cbcc74d3df07199c87cfb1fdce7be606a0e3a
Jack Kasbeer <jkasbeer2@gmail.com>
2025-04-17 19:57:21 -0700

    solutions added: .env & Meeting restful api implementation

---
+++ Client/src/views/admin/meeting/components/Addmeeting.js
@@ -0,0 +1,251 @@
+import { Button, Flex, FormLabel, Grid, GridItem, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Radio, RadioGroup, Stack, Text, Textarea } from '@chakra-ui/react';
+import { CUIAutoComplete } from 'chakra-ui-autocomplete';
+import MultiContactModel from 'components/commonTableModel/MultiContactModel';
+import MultiLeadModel from 'components/commonTableModel/MultiLeadModel';
+import Spinner from 'components/spinner/Spinner';
+import dayjs from 'dayjs';
+import { useFormik } from 'formik';
+import { useEffect, useState } from 'react';
+import { LiaMousePointerSolid } from 'react-icons/lia';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { MeetingSchema } from 'schema';
+import { getApi, postApi } from 'services/api';
+
+const AddMeeting = (props) => {
+    const { onClose, isOpen, setAction, from, fetchData, id, view, data } = props
+    const [leaddata, setLeadData] = useState([])
+    const [contactdata, setContactData] = useState([])
+    const [isLoding, setIsLoding] = useState(false)
+    const [contactModelOpen, setContactModel] = useState(false);
+    const [leadModelOpen, setLeadModel] = useState(false);
+    const todayTime = new Date().toISOString().split('.')[0];
+    const leadData = useSelector((state) => state?.leadData?.data);
+
+
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const contactList = useSelector((state) => state?.contactData?.data)
+
+
+    const initialValues = {
+        agenda: '',
+        attendes: props.leadContect === 'contactView' && id ? [id] : [],
+        attendesLead: props.leadContect === 'leadView' && id ? [id] : [],
+        location: '',
+        related: props.leadContect === 'contactView' ? 'Contact' : props.leadContect === 'leadView' ? 'Lead' : 'None',
+        dateTime: '',
+        notes: '',
+        createBy: user?._id,
+    }
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: MeetingSchema,
+        onSubmit: (values, { resetForm }) => {
+            AddData();
+            setAction(prev => !prev);
+            resetForm();
+            onClose();
+        },
+    });
+    const { errors, touched, values, handleBlur, handleChange, handleSubmit, setFieldValue } = formik
+
+
+    /*
+        Logic to add a new meeting from Client via restful
+    */
+    const AddData = async () => {
+        try {
+            setIsLoding(true);
+            let response = await postApi('api/meeting/add', values);
+            if (response.status === 200) {
+                fetchAllData(1);
+            }
+        } catch (e) {
+            console.log("error adding meeeting: ", e);
+        } finally {
+            setIsLoding(false);
+        }
+    };
+
+    // READY FOR DOUBLE CHECK
+    // Meetings home page is not immediately populating with new meeting after creation
+    // - need to check if this is directly from pre-written code or something i need to do
+    const fetchAllData = async () => {
+        if (id) {
+            try {
+                setIsLoding(true);
+                let result = await getApi('api/meeting/', id);
+                setFieldValue('agenda', result?.data?.agenda);
+                setFieldValue('location', result?.data?.location);
+                setFieldValue('related', result?.data?.related);
+                setFieldValue('dateTime', result?.data?.dateTime);
+                setFieldValue('notes', result?.data?.notes);
+            } catch (e) {
+                console.log("error fetching meeting data: ", e);
+            } finally {
+                setIsLoding(false);
+            }
+        } else if (data) {
+            setFieldValue('agenda', data?.agenda);
+            setFieldValue('location', data?.location);
+            setFieldValue('related', data?.related);
+            setFieldValue('dateTime', data?.dateTime);
+            setFieldValue('notes', data?.notes);
+        }
+    }
+
+    const handleMeetingAdded = () => {
+        fetchAllData();
+    }
+
+    useEffect(() => {
+        fetchAllData();
+    }, [props.id, values.related])
+
+    const extractLabels = (selectedItems) => {
+        return selectedItems.map((item) => item._id);
+    };
+
+    const countriesWithEmailAsLabel = (values.related === "Contact" ? contactdata : leaddata)?.map((item) => ({
+        ...item,
+        value: item._id,
+        label: values.related === "Contact" ? `${item.firstName} ${item.lastName}` : item.leadName,
+    }));
+
+    return (
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent height={"580px"}>
+                <ModalHeader>Add Meeting </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody overflowY={"auto"} height={"400px"}>
+                    {/* Contact Model  */}
+                    <MultiContactModel data={contactdata} isOpen={contactModelOpen} onClose={setContactModel} fieldName='attendes' setFieldValue={setFieldValue} />
+                    {/* Lead Model  */}
+                    <MultiLeadModel data={leaddata} isOpen={leadModelOpen} onClose={setLeadModel} fieldName='attendesLead' setFieldValue={setFieldValue} />
+
+                    <Grid templateColumns="repeat(12, 1fr)" gap={3}>
+                        <GridItem colSpan={{ base: 12 }}>
+                            <FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' mb='8px'>
+                                Agenda<Text color={"red"}>*</Text>
+                            </FormLabel>
+                            <Input
+                                fontSize='sm'
+                                onChange={handleChange} onBlur={handleBlur}
+                                value={values.agenda}
+                                name="agenda"
+                                placeholder='Agenda'
+                                fontWeight='500'
+                                borderColor={errors.agenda && touched.agenda ? "red.300" : null}
+                            />
+                            <Text fontSize='sm' mb='10px' color={'red'}> {errors.agenda && touched.agenda && errors.agenda}</Text>
+                        </GridItem>
+                        <GridItem colSpan={{ base: 12 }} >
+                            <FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' mb='8px'>
+                                Related To<Text color={"red"}>*</Text>
+                            </FormLabel>
+                            <RadioGroup onChange={(e) => setFieldValue('related', e)} value={values.related}>
+                                <Stack direction='row'>
+                                    {props.leadContect === 'contactView' && <Radio value='Contact'>Contact</Radio>}
+                                    {props.leadContect === 'leadView' && <Radio value='Lead'>Lead</Radio>}
+                                    {!props.leadContect && <> <Radio value='Contact'>Contact</Radio><Radio value='Lead'>Lead</Radio></>}
+                                </Stack>
+                            </RadioGroup>
+                            <Text mb='10px' color={'red'} fontSize='sm'> {errors.related && touched.related && errors.related}</Text>
+                        </GridItem>
+                        {(values.related === "Contact" ? (contactdata?.length ?? 0) > 0 : (leaddata?.length ?? 0) > 0) && values.related &&
+
+                            <GridItem colSpan={{ base: 12 }}>
+                                <Flex alignItems={'end'} justifyContent={'space-between'} >
+                                    <Text w={'100%'} >
+                                        <CUIAutoComplete
+                                            label={`Choose Preferred Attendes ${values.related === "Contact" ? "Contact" : values.related === "Lead" && "Lead"}`}
+                                            placeholder="Type a Name"
+                                            name="attendes"
+                                            items={countriesWithEmailAsLabel}
+                                            className='custom-autoComplete'
+                                            selectedItems={countriesWithEmailAsLabel?.filter((item) => values.related === "Contact" ? values?.attendes.includes(item._id) : values.related === "Lead" && values?.attendesLead.includes(item._id))}
+                                            onSelectedItemsChange={(changes) => {
+                                                const selectedLabels = extractLabels(changes.selectedItems);
+                                                values.related === "Contact" ? setFieldValue('attendes', selectedLabels) : values.related === "Lead" && setFieldValue('attendesLead', selectedLabels)
+                                            }}
+                                        />
+                                    </Text>
+                                    <IconButton mb={6} onClick={() => values.related === "Contact" ? setContactModel(true) : values.related === "Lead" && setLeadModel(true)} fontSize='25px' icon={<LiaMousePointerSolid />} />
+                                </Flex>
+                                <Text color={'red'}> {errors.attendes && touched.attendes && errors.attendes}</Text>
+                            </GridItem>
+                        }
+                        <GridItem colSpan={{ base: 12 }}>
+                            <FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' mb='8px'>
+                                Location
+                            </FormLabel>
+                            <Input
+                                fontSize='sm'
+                                onChange={handleChange} onBlur={handleBlur}
+                                value={values.location}
+                                name="location"
+                                placeholder='Location'
+                                fontWeight='500'
+                                borderColor={errors.location && touched.location ? "red.300" : null}
+                            />
+                            <Text mb='10px' color={'red'} fontSize='sm'> {errors.location && touched.location && errors.location}</Text>
+                        </GridItem>
+                        <GridItem colSpan={{ base: 12 }}>
+                            <FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' mb='8px'>
+                                Date Time<Text color={"red"}>*</Text>
+                            </FormLabel>
+                            <Input
+                                fontSize='sm'
+                                type='datetime-local'
+                                onChange={handleChange} onBlur={handleBlur}
+                                min={dayjs(todayTime).format('YYYY-MM-DD HH:mm')}
+                                value={values.dateTime}
+                                name="dateTime"
+                                placeholder='Date Time'
+                                fontWeight='500'
+                                borderColor={errors.dateTime && touched.dateTime ? "red.300" : null}
+                            />
+                            <Text fontSize='sm' mb='10px' color={'red'}> {errors.dateTime && touched.dateTime && errors.dateTime}</Text>
+                        </GridItem>
+                        <GridItem colSpan={{ base: 12 }}>
+                            <FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' mb='8px'>
+                                Notes
+                            </FormLabel>
+                            <Textarea
+                                resize={'none'}
+                                fontSize='sm'
+                                placeholder='Notes'
+                                onChange={handleChange} onBlur={handleBlur}
+                                value={values.notes}
+                                name="notes"
+                                fontWeight='500'
+                                borderColor={errors.notes && touched.notes ? "red.300" : null}
+                            />
+                            <Text mb='10px' color={'red'}> {errors.notes && touched.notes && errors.notes}</Text>
+                        </GridItem>
+
+                    </Grid>
+
+
+                </ModalBody>
+                <ModalFooter>
+                    <Button size="sm" variant='brand' me={2} disabled={isLoding} onClick={handleSubmit}>{isLoding ? <Spinner /> : 'Save'}</Button>
+                    <Button sx={{
+                        textTransform: "capitalize",
+                    }} variant="outline"
+                        colorScheme="red" size="sm" onClick={() => {
+                            formik.resetForm()
+                            onClose()
+                        }}>Close</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default AddMeeting
+
+
---
+++ server/controllers/meeting/meeting.js
@@ -0,0 +1,214 @@
+const MeetingHistory = require('../../model/schema/meeting')
+const mongoose = require('mongoose');
+
+/*
+    Add a meeting to the list
+ */
+const add = async (req, res) => {
+    try {
+        const { agenda, location, related, dateTime, notes, createBy, attendes, attendesLead } = req.body;
+        // Check if each attende is a valid ObjectId (if provided)
+        for (const id of attendes) {
+            if (id && !mongoose.Types.ObjectId.isValid(id))
+                res.status(440).json({ error: 'Invalid attendes value: ', attende });
+        }
+        // Check if each attendesLead is a valid ObjectId (if provided)
+        for (const id of attendesLead) {
+            if (id && !mongoose.Types.ObjectId.isValid(id))
+                res.status(440).json({ error: 'Invalid attendesLead value: ', attende});
+        }
+        const meetingData = { 
+            agenda, 
+            location, 
+            related, 
+            dateTime, 
+            notes, 
+            createBy,
+            timestamp: new Date(),
+        };
+
+        if (attendes) {
+            meetingData.attendes = attendes;
+        }
+        if (attendesLead) {
+            meetingData.attendesLead = attendesLead;
+        }
+        const result = new MeetingHistory(meetingData);
+        await result.save();
+        res.status(201).json(result);
+    } catch (err) {
+        console.error('Failed to create task:', err);
+        res.status(404).json({ error: 'Failed to create meeting: ', err });
+    }
+}
+
+/*
+    View the meeting page overview
+ */
+const index = async (req, res) => {
+    query = req.query;
+    query.deleted = false;
+    if (query.createBy) {
+        query.createBy = new mongoose.Types.ObjectId(query.createBy);
+    }
+    try {
+        let result = await MeetingHistory.aggregate([
+            { $match: query },
+            {
+                $lookup: {
+                    from: 'Contacts',
+                    localField: 'attendes',
+                    foreignField: '_id',
+                    as: 'contact'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'Leads',
+                    localField: 'attendesLead',
+                    foreignField: '_id',
+                    as: 'Lead'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'User',
+                    localField: 'createBy',
+                    foreignField: '_id',
+                    as: 'users'
+                }
+            },
+            { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$contact', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$Lead', preserveNullAndEmptyArrays: true } },
+            { $match: { 'users.deleted': false } },
+            {
+                $addFields: {
+                    createByName: '$user.username',
+                    attendesName: {
+                        $cond: {
+                            if: '$contact',
+                            then: { $concat: ['$contact.title', ' ', '$contact.firstName', ' ', '$contact.lastName'] },
+                            else: '$lead.leadName'
+                        }
+                    }
+                }
+            },
+            { $project: { users: 0, contact: 0, Lead: 0 } },//DON'T KNOW IF CORRECT
+            
+        ]);
+        res.send(result);
+    } catch (err) {
+        console.error('Error: ', err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+/*
+    View a single meeting
+ */
+const view = async (req, res) => {
+    try {
+        let response = await MeetingHistory.findOne({ _id: req.params.id });
+        if (!response) return res.status(404).json({ message: 'no meeting Data Found.'});
+        let result = await MeetingHistory.aggregate([
+            { $match: { _id: response._id } },
+            {
+                $lookup: {
+                    from: 'Contacts',
+                    localField: 'attendes',
+                    foreignField: '_id',
+                    as: 'contact'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'Leads',
+                    localField: 'attendesLead',
+                    foreignField: '_id',
+                    as: 'Lead'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'User',
+                    localField: 'createBy',
+                    foreignField: '_id',
+                    as: 'users'
+                }
+            },
+            { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$contact', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$Lead', preserveNullAndEmptyArrays: true } },
+            { $match: { 'users.deleted': false } },
+            {
+                $addFields: {
+                    createByName: '$user.username',
+                    attendesName: {
+                        $cond: {
+                            if: '$contact',
+                            then: { $concat: ['$contact.title', ' ', '$contact.firstName', ' ', '$contact.lastName'] },
+                            else: '$lead.leadName'
+                        }
+                    }
+                }
+            },
+            { $project: { contact: 0, users: 0, Lead: 0 } },
+        ]);
+        res.status(200).json(result[0]);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(400).json({ Error: err });
+    }
+}
+
+/*
+    Delete a particular meeting
+ */
+const deleteData = async (req, res) => {
+    try {
+        const result = await MeetingHistory.findByIdAndUpdate(req.params.id, { deleted: true });
+        res.status(200).json({ 
+            message: 'done: meeting soft deleted', 
+            result 
+        });
+    } catch (err) {
+        res.status(404).json({ 
+            message: 'meeting delete error', 
+            err 
+        });
+    }
+}
+
+/*
+    Delete multiple meetings
+ */
+const deleteMany = async (req, res) => {
+    try {
+        const result = await MeetingHistory.updateMany(
+            { _id: { $in: req.body } }, 
+            { $set: { deleted: true } }
+        );
+
+        if (result?.matchedCount > 0 && result?.modifiedCount > 0) {
+            return res.status(200).json({ 
+                message: "Meetings Removed successfully", 
+                result 
+            });
+        } 
+        else {
+            return res.status(404).json({ 
+                success: false, 
+                message: "Failed to remove Meetings"
+            });
+        }
+    } catch (err) {
+        return res.status(404).json({ 
+            success: false, 
+            message: 'error deleting Meetings', 
+            error: err, 
+        });
+    }
+}
+
+module.exports = { add, index, view, deleteData, deleteMany }
